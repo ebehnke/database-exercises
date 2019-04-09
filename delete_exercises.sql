9dead8f27237dc285377ae00ae8796a5a7b0ed4b
Erik Behnke <erik.behnke@gmail.com>
2019-04-09 14:37:11 -0500

    get to delete portion of exercise

---
+++ delete_exercises.sql
@@ -0,0 +1,15 @@
+use codeup_test_db;
+
+select name from albums where release_year '1991'>;
+delete name from albums where release_year '1991'>;
+select name from albums where release_year '1991'>;
+
+
+select name from albums where genre <= 'disco';
+delete name from albums where genre <= 'disco';
+select name from albums where genre <= 'disco';
+
+
+select name from albums where artist = 'ABBA';
+delete name from albums where artist = 'ABBA';
+select name from albums where artist = 'ABBA';
